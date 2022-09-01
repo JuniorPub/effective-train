83c5b542c7d64d75f3bf6fbd329fb91bfaf13ab3
JuniorPub <junior_c@outlook.dk>
2022-09-01 12:53:45 +0200

    Done with login

--- Maze Runner/Main.js
+++ Maze Runner/Main.js
@@ -21,11 +21,11 @@ function calc() {
 function validate() {
     var username=document.getElementById("username").value;
     var password=document.getElementById("password").value;
-if(username=="informatik" & password == "1234") {
+    
+    if(username=="informatik" && password == "1234") {
     alert("login succesfully");
-    return false;
 }
-else {
+    else {
     alert("faill to login")
 }
 }
