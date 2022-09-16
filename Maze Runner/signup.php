cef2fba81a13f7631dcb9f11ffcdbbe77f39dd71
JuniorPub <junior_c@outlook.dk>
2022-09-16 13:45:05 +0200

    de

--- Maze Runner/signup.php
+++ Maze Runner/signup.php
@@ -1,15 +1,16 @@
 <?php 
     include_once 'header.php';
 ?>
+
+
     <section class="signup-form">
-        
         <div id="signup" class="signup-form-form">
-            <form class="box" action="signup.inc.php" method="post">
+            <form class="box"  action="includes/signup.inc.php" method="post">
                 <h1> Sign Up </h1>
                 <input type="text" name="uid" placeholder="Brugernavn"> 
                 <input type="password" name="pwd" placeholder="Password">
                 <input type="password" name="pwdrepeat" placeholder="Repeat password">
-                <button type="button" onclick="login()">Login</button>
+                <button type="submit" name="submit">Sign Up</button>
             </form>
         </div>
     </section>
